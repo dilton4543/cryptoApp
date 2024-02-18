d770b3cb99a1851005cf52e80292059b093cd743
dilton4543 <loteesenwah.17@gmail.com>
2024-02-18 21:46:22 +0100

    added whileloop functionality

--- Testing git/features.js
+++ Testing git/features.js
@@ -5,7 +5,10 @@ for(let i=0; i<x.length; i++){
     console.log(x[i]);
 }
 
-
+while (i < x.length) {
+    console.log(x[i]);
+    i++; // Increment the counter inside the loop
+  }
 
 document.getElementById('test').innerHTML = 'Testing checkout, Revert and Reset commit was edited by javascript.';
 
