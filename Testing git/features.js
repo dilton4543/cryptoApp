b19137de6302817945fbe5ecabe54f994c5b55ab
dilton4543 <loteesenwah.17@gmail.com>
2024-02-18 21:43:45 +0100

    added index44 and style44 with a features.js

---
+++ Testing git/features.js
@@ -0,0 +1,12 @@
+console.log('Hello there');
+const x =['lota','benji','samuel','ike'];
+
+for(let i=0; i<x.length; i++){
+    console.log(x[i]);
+}
+
+
+
+document.getElementById('test').innerHTML = 'Testing checkout, Revert and Reset commit was edited by javascript.';
+
+document.write('hello world!');
